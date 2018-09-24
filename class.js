b8cc7a6e3b394b810757fe8994c88647dd208151
WOLFRIEND <41586082+WOLFRIEND@users.noreply.github.com>
2018-09-25 00:05:01 +0400

    Adding files

---
+++ class.js
@@ -0,0 +1,7 @@
+export default class Phones {
+  constructor(brand, model) {
+    this.brand = brand;
+    this.model = model;
+  }
+}
+
---
+++ function.js
@@ -0,0 +1,7 @@
+function buyPhone(phones, budget) {
+  console.log(`If you want buy ${phones.brand} ${phones.model} you should have:
+    not less than ${budget} $`);
+}
+
+export { buyPhone };
+
